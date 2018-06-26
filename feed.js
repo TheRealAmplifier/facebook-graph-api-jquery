5b0f0f1976c27c771aff71d286d883ae2d328843
TheRealAmplifier <coding@jeroenpielage.nl>
2018-06-26 16:50:37 +0200

    Added font and structure for getting the data

--- feed.js
+++ feed.js
@@ -1,11 +1,10 @@
 
-	alert('test');
 	let postlock, description;
 
-	const appId = 'app id here!';
+	const appId = '202892533681430';
 
 	const feedFields = 'feed.limit(10){type,from,message,description,story,permalink_url,place,created_time,attachments{title,url,media,type,description,subattachments}}';
-	const feedToken = 'feed token here!';
+	const feedToken = 'EAADYZBPXF4ZB0BADdQwMyhgtu6o1ZBYwHzxvZCZAcPhWXlSzZBtLqSJp6KwjicjT4CXIQVWmyu5NLltcN3IjyhlWqkZAUA7tMQZBrDqx1uuH1hqpWwpPq3YZCXd85A8PN7O3Y6IhHxUYziTIC6jhjQPLswT3w3ZBCvAhiG3PVP37ZAwHQZDZD';
 
 	window.fbAsyncInit = function() {
 		FB.init({
@@ -16,7 +15,41 @@
 		});
 		FB.api(appId, { fields : feedFields, access_token : feedToken }, function(response) {
 			console.log('App is being loaded...');
-			console.log(response);
+			console.dir(response);
+
+			let feedData = response;
+
+			$.each(feedData, function(index) {
+				$.each(this, function(globalKey, globalValue) {
+					if(globalKey == 'data') {
+						$.each(globalValue, function(dataKey, dataValue){
+							console.log(dataValue.type);
+
+							if(dataValue.type == 'post') {
+								console.log('default post');
+								$.each(dataValue, function(detailKey, detailValue) {
+									console.log(`${detailKey} belongs to ${detailValue}`);
+								});
+							} else if (dataValue.type  == 'photo') {
+								console.log('photo post');
+								$.each(dataValue, function(detailKey, detailValue) {
+									console.log(`${detailKey} belongs to ${detailValue}`);
+								});
+							} else if (dataValue.type  == 'status' ) {
+								console.log('status update post');
+								$.each(dataValue, function(detailKey, detailValue) {
+									console.log(`${detailKey} belongs to ${detailValue}`);
+								});
+							} else {
+								console.log('Everything else...');
+								$.each(dataValue, function(detailKey, detailValue) {
+									console.log(`${detailKey} belongs to ${detailValue}`);
+								});
+							}
+						});
+					}
+				});
+			});
 		});
 	}
 
