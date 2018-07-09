042901f295c31897cbc63b2e00b0e983c7ecc7dd
TheRealAmplifier <coding@jeroenpielage.nl>
2018-07-09 22:15:32 +0200

    Improved photo feed

--- feed.js
+++ feed.js
@@ -25,7 +25,7 @@
 
 					if(globalKey == 'data') {
 						$.each(globalValue, function(dataKey, dataValue){
-							console.log(dataValue);
+							//console.dir(dataValue);
 
 							//
 							// To Do
@@ -35,33 +35,117 @@
 							postblock = '<div class="facebook-post '+dataValue.type+'">';
 								if(dataValue.type == 'post') {
 									postblock += "<div class='post'>";								
-										postblock += '<span>'+dataValue.type+'</span>';
-										postblock += '<span>'+dataValue.id+'</span>';
-										postblock += '<span>'+dataValue.message+'</span>';
+										if( dataValue.message != undefined) {
+											postblock += `<span class='message'>${dataValue.message}</span>`;
+										} else {
+											postblock += `<span class='message'>Oops, message could not be loaded...</span>`;
+										}
+									postblock += '</div>';
+								} else if (dataValue.type  == 'photo') {		// get status type post
+									postblock += "<div class='post'>";
+											$.each(this, function(keyAttachment, valueAttachment) {
+												if (keyAttachment == "attachments") {
+													$.each(valueAttachment, function(keyAttachment2, valueAttachment2) {
+														$.each(valueAttachment2, function(keyAttachment3, valueAttachment3) {
+															let post_title = valueAttachment3.title;
+															let post_url = valueAttachment3.url;
+															console.log(valueAttachment3);
+
+															if(valueAttachment3.type == 'event' || valueAttachment3.type == 'life_event'|| valueAttachment3.type == 'cover_photo'){
+																$.each(valueAttachment3, function(keyAttachment4, valueAttachment4) {
+																	$.each(valueAttachment4, function(keyAttachment5, valueAttachment5) {
+																		if (keyAttachment5 == "image") {
+																			postblock += `<a href="${post_url}" target="_blank">`;
+																			postblock += `<span class='post-image'><img src="${valueAttachment5.src}" alt="${post_title}" />`;
+																			postblock += `</a>`;
+																		}
+																	});
+																});
+															} else if(value_attachment3.type == 'album'){
+
+														
+			
+												// 			// 	$.each(value_attachment3, function(key_attachment4, value_attachment4) {
+
+												// 			// 		if(key_attachment4 == 'subattachments'){
+												// 			// 		$.each(value_attachment4, function(key_attachment5, value_attachment5) {
+																	
+												// 			// 				$.each(value_attachment5, function(key_attachment6, value_attachment6) {
+												// 			// 					//console.log(value_attachment6);
+												// 			// 					if( value_attachment6.url != undefined){ 
+		
+												// 			// 						post_block += "<a target='_blank' href='" + value_attachment6.url + "'>" ;
+		
+												// 			// 						$.each(value_attachment6, function(key_attachment7, value_attachment7) {
+		
+												// 			// 							if(key_attachment7 == 'media'){
+												// 			// 								$.each(value_attachment7, function(key_attachment8, value_attachment8) {
+												// 			// 									//console.log(key_attachment8, value_attachment8); 
+												// 			// 									post_block += "<img class='image' src='" + value_attachment8.src + "' alt='" + alt_title + "'/>" ;
+												// 			// 								});
+												// 			// 							}
+												// 			// 						});
+		
+												// 			// 						post_block += "</a>" ;	
+												// 			// 					}
+												// 			// 				});
+												// 			// 		});
+												// 			// 	}
+												// 			// 	});
+																
+																
+												// 			// } else if(value_attachment3.type == 'share'){
+																
+																
+		
+																	// 	share_description = value_attachment3.description;
+																	// 	share_url = value_attachment3.url;
+																	// 	share_alt = value_attachment3.title;
+				
+																	// 	$.each(value_attachment3, function(key_attachment4, value_attachment4) {
+																	// 		// console.log(value_attachment4);															
+																	// 			$.each(value_attachment4, function(key_attachment5, value_attachment5) {
+				
+		
+																			// if (key_attachment5 == "image") {
+																			// 	post_block += "<article>" ;
+																			// 		post_block += "<a target='_blank' href='" + share_url + " alt=" + share_alt + " '>" ;
+																			// 			post_block += "<img class='share image' src='" + value_attachment5.src + "' alt='" + alt_title + "'/>" ;
+																			// 			post_block += "<span class='share message'>" + share_description + "'</span>" ;
+																			// 		post_block += "</a>" ;
+																			// 	post_block += "</article>" ;
+																			// }
+																// 	});						
+																//});	
+															}
+														});
+													});
+												}
+											});
+									postblock += '</div>';
+								} else if (dataValue.type  == 'status' ) {	// get pictures date type status
+									postblock += "<div class='post'>";
+										if( dataValue.message != undefined) {
+											postblock += `<span class='message'>${dataValue.message}</span>`;
+										} else {
+											postblock += `<span class='message'>Oops, message could not be loaded...</span>`;
+										}
 									postblock += '</div>';
 
-									// get status type post
-								} else if (dataValue.type  == 'photo') {
+									// get status type data
+								} else if (dataValue.type  == 'event' ) {
 									postblock += "<div class='post'>";
-										postblock += '<span>'+dataValue.type+'</span>';
-										postblock += '<span>'+dataValue.id+'</span>';
-										postblock += '<span>'+dataValue.message+'</span>';
-									postblock += '</div>';
-
-									// get pictures date type photo
-								} else if (dataValue.type  == 'status' ) {
-									postblock += "<div class='post'>";
-										postblock += '<span>'+dataValue.type+'</span>';
-										postblock += '<span>'+dataValue.id+'</span>';
-										postblock += '<span>'+dataValue.message+'</span>';
+										postblock += `<span>${dataValue.type}</span>`;
+										
+										postblock += `<span>${dataValue.message}</span>`;
 									postblock += '</div>';
 
 									// get status type data
 								} else {
 									postblock += "<div class='post'>";
-										postblock += '<span>'+dataValue.type+'</span>';
-										postblock += '<span>'+dataValue.id+'</span>';
-										postblock += '<span>'+dataValue.message+'</span>';
+										postblock += `<span>${dataValue.type}</span>`;
+
+										postblock += `<span>${dataValue.message}</span>`;
 									postblock += '</div>';
 
 									// more data, every other post
